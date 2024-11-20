6310c397ae50d167b694d013a36575918989b697
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-11-20 03:54:58 +0100

    commit

--- frontend/src/app-configurations.ts
+++ frontend/src/app-configurations.ts
@@ -2,7 +2,7 @@ export const configurations = {
     appName: "Savor Morocco",
     developer:"Anas ELMOUDEN",
     bluetheme: "rgb(94, 123, 238)",
-    host: "http://localhost:5173",
+    host: "https://savor-morocco.vercel.app",
     appDescription:"Savor Morocco is a web app that provides users with a rich collection of Moroccan recipes. The app offers a range of features to enhance the cooking experience",
     userDefaultPic:"https://firebasestorage.googleapis.com/v0/b/recipes-app-d9a48.firebasestorage.app/o/user.png?alt=media&token=b3e30e6c-d03e-46e1-827f-98a5a226f468",
     emailAdress: "noreply@trueflowing.com",
--- frontend/src/http-common.ts
+++ frontend/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://savor-morocco-backend.vercel.app/api",
   headers: {
     "Content-type": "application/json"
   }
