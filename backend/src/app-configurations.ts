9bb2524b694a8689ad0b4d84e45bd0d3fa552883
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2024-11-20 03:17:57 +0100

    commit

--- backend/src/app-configurations.ts
+++ backend/src/app-configurations.ts
@@ -2,7 +2,7 @@ export const configurations = {
     appName: "Savor Morocco",
     developer:"Anas ELMOUDEN",
     bluetheme: "rgb(94, 123, 238)",
-    host: "http://localhost:5000",
+    host: "https://savor-morocco-backend.vercel.app",
     emailAdress: "noreply@trueflowing.com",
     domain: "trueflowing.com"
   };
